6abe1876015809bfbaef05aaf5192611d04ad51e
Andika Saputra <andika.3206@students.amikom.ac.id>
2017-05-19 14:38:50 +0700

    delete post

---
+++ admin/page/posts/delete.php
@@ -0,0 +1,10 @@
+<?php 
+if (!isset($_GET['id'])) {
+	header('location: ?page=posts/index');
+}
+$id = htmlspecialchars($_GET['id']);
+$query = "DELETE FROM posts WHERE id = $id";
+$sql = mysqli_query($koneksi, $query) or die (mysqli_error($koneksi));
+if ($sql) {
+	header('location: ?page=posts/index');
+}
