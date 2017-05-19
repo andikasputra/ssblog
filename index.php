28a3a3eef9b54943413cebe6b8af1b7d6b908b51
Andika Saputra <andika.3206@students.amikom.ac.id>
2017-05-19 14:55:24 +0700

    homepage

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php include 'layouts/header.php' ?>
 <div class="container-fluid">
 	<div class="row fh5co-post-entry">
+		<?php 
+		$query = "SELECT posts.*, categories.name, user.fullname FROM posts JOIN categories ON posts.category_id = categories.id JOIN user ON posts.user_id = user.id ORDER BY posts.id DESC";
+		$sql = mysqli_query($koneksi, $query) or die (mysqli_error($koneksi) . $query);
+		while ($post = mysqli_fetch_assoc($sql)) { ?>
 		<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
 			<figure>
-				<a href="single.html"><img src="images/pic_1.jpg" alt="Image" class="img-responsive"></a>
+				<a href="single.html"><img src="assets/upload/images/<?= $post['picture'] ?>" alt="<?= $post['title'] ?>" class="img-responsive"></a>
 			</figure>
-			<span class="fh5co-meta"><a href="single.html">Food &amp; Drink</a></span>
-			<h2 class="fh5co-article-title"><a href="single.html">We Eat and Drink All Night</a></h2>
-			<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			<span class="fh5co-meta"><a href="single.html"><?= $post['name'] ?></a></span>
+			<h2 class="fh5co-article-title"><a href="baca-<?= $post['slug'] ?>"><?= $post['title'] ?></a></h2>
+			<span class="fh5co-meta fh5co-date"><?= date('F d Y', strtotime($post['date'])) ?></span>
 		</article>
+		<?php 
+		} ?>
 		<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
 			<figure>
 				<a href="single.html"><img src="images/pic_2.jpg" alt="Image" class="img-responsive"></a>
--- layouts/header.php
+++ layouts/header.php
@@ -1,4 +1,6 @@
-
+<?php 
+session_start();
+include 'conf/db.php'; ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
